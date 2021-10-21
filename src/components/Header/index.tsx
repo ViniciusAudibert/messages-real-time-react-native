15898a41e9da110074d58cfbc7a9eef0c3103a1a
Vinicius Audibert <vini.audibert@gmail.com>
2021-10-21 00:26:52 -0300

    feat: adicionar header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Text, TouchableOpacity, View } from 'react-native'
+
+import { styles } from './styles'
+
+import LogoSvg from '../../assets/logo.svg'
+import { UserPhoto } from '../UserPhoto'
+
+export function Header() {
+  return (
+    <View style={styles.container}>
+      <LogoSvg />
+
+      <View style={styles.logoutButton}>
+        <TouchableOpacity>
+          <Text style={styles.logoutText}>Sair</Text>
+        </TouchableOpacity>
+
+        <UserPhoto imageUri="https://github.com/viniciusaudibert.png" />
+      </View>
+    </View>
+  )
+}
